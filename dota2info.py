9fe1e23ab0491c471ff7b407efae5151172b6b98
Roman Kuzmichev <daerdemandt@gmail.com>
2016-02-05 22:47:14 +0300

    Added half of get_heroes_list method

--- dota2info.py
+++ dota2info.py
@@ -1,12 +1,23 @@
+import urllib.request, json
+
+
 class Dota2info:
 	"Main class responsible for obtaining info"
 	def __init__(self, address):
-		pass
-	def get_heroes_list(self, language = 'english'):
-		raise NotImplementedError
+		self.address = address
+
+	def get_heroes_list(self, language = 'english'): #TODO: add images
+		url = 'http://' + self.address + '/jsfeed/heropickerdata?l=' + language
+		return self.get_json(url)
+
 	def get_hero_details(self, hero_id, language = 'english'):
 		raise NotImplementedError
 	def get_items_list(self, language = 'english'):
 		raise NotImplementedError
 	def get_item_details(self, item_id, language = 'english'):
 		raise NotImplementedError
+
+	def get_json(self, url):
+		response = urllib.request.urlopen(url).read().decode('utf8')
+		data = json.loads(response)
+		return data
